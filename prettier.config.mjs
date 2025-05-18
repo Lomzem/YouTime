6ca196a898d3f9d2a49f9e3de359f31cbcf90a8f
Lawjay Lee <lawjaylee1@icloud.com>
2025-05-17 19:01:47 -0700

    Add prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    semi: false,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "es5",
+    useTabs: false,
+}
+
+export default config

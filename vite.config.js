83a6a4ca8aa37d7d55a03fe855127c6a1e81a199
Lawjay Lee <lawjaylee1@icloud.com>
2025-05-19 16:07:46 -0700

    Use default vite config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,36 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 /** @type {import('vite').UserConfig} */
 export default {
+    compilerOptions: {
+        target: "ES2020",
+        useDefineForClassFields: true,
+        module: "ESNext",
+        lib: ["ES2020", "DOM", "DOM.Iterable"],
+        skipLibCheck: true,
+
+        /* Bundler mode */
+        moduleResolution: "bundler",
+        allowImportingTsExtensions: true,
+        verbatimModuleSyntax: true,
+        moduleDetection: "force",
+        noEmit: true,
+
+        /* Linting */
+        strict: true,
+        noUnusedLocals: true,
+        noUnusedParameters: true,
+        erasableSyntaxOnly: true,
+        noFallthroughCasesInSwitch: true,
+        noUncheckedSideEffectImports: true,
+    },
     build: {
         outDir: "dist",
         minify: false,
         rollupOptions: {
             input: {
                 background: "./src/background/background.ts",
-                getInput: "./src/content/getInput/getInput.ts",
                 vault: "./src/vault/vault.html",
+                getInput: "./src/content/getInput/getInput.ts",
             },
             output: {
                 entryFileNames: `[name].js`,

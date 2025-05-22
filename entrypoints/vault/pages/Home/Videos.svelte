3e985b4b18fc2b37f0b5438d9824360ae600872e
Lawjay Lee <lawjaylee1@icloud.com>
2025-05-21 21:46:54 -0700

    CSS cards same size

--- entrypoints/vault/pages/Home/Videos.svelte
+++ entrypoints/vault/pages/Home/Videos.svelte
@@ -4,23 +4,31 @@
 
 {#snippet videoCard(video: YTVideo)}
     <a href={`#/video/${getIDfromURL(video.url)}`}>
-        <li class="bg-foreground cursor-pointer overflow-hidden rounded-lg">
-            <div class="relative">
-                <img
-                    src={video.thumbnailUrl}
-                    alt="thumbnail"
-                    class="aspect-video w-full object-cover"
-                />
+        <li
+            class="group h-full cursor-pointer overflow-hidden rounded-lg transition-all duration-200 hover:scale-101"
+        >
+            <div class="flex h-full flex-col">
+                <div class="relative">
+                    <img
+                        src={video.thumbnailUrl}
+                        alt="thumbnail"
+                        class="aspect-video w-full object-cover"
+                    />
 
-                <time
-                    class="absolute right-2 bottom-2 rounded bg-black/70 px-2 py-1 text-sm text-white"
-                >
-                    <span>{video.timestamps.toSorted().pop()?.time}</span>
-                </time>
-            </div>
+                    <time
+                        class="absolute right-2 bottom-2 rounded bg-black/70 px-2 py-1 text-sm text-white"
+                    >
+                        <span>{video.timestamps.toSorted().pop()?.time}</span>
+                    </time>
+                </div>
 
-            <div class="line-clamp-2 p-3 font-medium">
-                <h3>{video.title}</h3>
+                <div class="bg-foreground flex flex-1 flex-col p-3">
+                    <h3 class="mb-2 line-clamp-2 font-medium">{video.title}</h3>
+
+                    <div class="text-copy-lighter mt-auto text-sm">
+                        <span>{video.timestamps.length} notes</span>
+                    </div>
+                </div>
             </div>
         </li>
     </a>
